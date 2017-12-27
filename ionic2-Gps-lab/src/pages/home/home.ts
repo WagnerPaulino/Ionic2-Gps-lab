82f96301abf9fe21ce00c5de0eba10c49da5eb91
Wagner Paulino <wagnersantos9778@gmail.com>
2017-12-27 10:03:08 -0300

    Geolocalização atraves de latitude e longitude

--- ionic2-Gps-lab/src/pages/home/home.ts
+++ ionic2-Gps-lab/src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,13 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private geolocation: Geolocation) {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      console.log(resp.coords.latitude);
+      console.log(resp.coords.longitude);
+     }).catch((error) => {
+       console.log('Error getting location', error);
+     });
   }
 
 }
